58ccd51261e68607a1f84d33d1c6b0e8a45621dd
ankitkh007 <ankit123khanna456@gmail.com>
2026-01-03 13:24:46 +0530

    Tested environment file

---
+++ test_env.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key=os.environ.get('API_KEY')
+database_url=os.environ.get("DATABASE_URL")
+print(f"The API KEY is: {api_key}")
+print(f"The Database URL is: {database_url}")
