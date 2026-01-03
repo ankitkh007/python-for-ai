beb8a8842427ad42e38c9d21202c1ea92b90c0d7
ankitkh007 <ankit123khanna456@gmail.com>
2026-01-03 14:35:16 +0530

    Formatted with Ruff

--- test.py
+++ test.py
@@ -1,5 +1,20 @@
-import requests
+# import requests
 
-# Download a web page
-response = requests.get("https://api.github.com")
-print(response.status_code)  # Should print 200 
+# # Download a web page
+# response = requests.get("https://api.github.com")
+# print(response.status_code)  # Should print 200
+
+
+def calculate_total(items):
+    total = 0
+    for item in items:
+        total += item["price"] * item["quantity"]
+    return total
+
+
+shopping_cart = [
+    {"name": "apple", "price": 0.5, "quantity": 6},
+    {"name": "banana", "price": 0.3, "quantity": 8},
+]
+
+print(calculate_total(shopping_cart))
--- test_env.py
+++ test_env.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-api_key=os.environ.get('API_KEY')
-database_url=os.environ.get("DATABASE_URL")
+api_key = os.environ.get("API_KEY")
+database_url = os.environ.get("DATABASE_URL")
 print(f"The API KEY is: {api_key}")
 print(f"The Database URL is: {database_url}")
